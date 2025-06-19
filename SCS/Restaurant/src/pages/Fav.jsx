1bbf8a88a12f5bdee9f2179ad332ccd26c0f761a
Lucy <solouser019@gmail.com>
2025-06-19 23:38:32 +0530

    rewrite code

--- SCS/Restaurant/src/pages/Fav.jsx
+++ SCS/Restaurant/src/pages/Fav.jsx
@@ -6,11 +6,12 @@ const favrate =  JSON.parse(localStorage.getItem("fav") || []);
 const renderrecipes = favrate.map((recipe) => (
   <RecipeCard key={recipe.id} recipe={recipe} />
 ))
+
   return (
     <div className='flex flex-wrap  '>{favrate.length > 0 ? renderrecipes : "No Favrate found!"}</div>
   )
 
-  
+
 }
 
 export default Fav
