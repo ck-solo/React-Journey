e31ddd323e3d2cfcb95b7e56a4ccbefdbcd3a880
Lucy <solouser019@gmail.com>
2025-10-01 01:05:33 +0530

    Learned map() in React and rendered JSON data into components

---
+++ Cohort/Day3_TwoWayBinding/src/Map.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Map = () => {
+    const profile =[
+        {
+            name: 'Shriyasmh',
+            age: 25,
+            gender: 'Male',
+        },
+        {
+            name: 'Rahul',
+            age: 25,
+            gender: 'Male',
+        },{
+            name: 'Ruhani',
+            age: 295,
+            gender: 'Female',
+        }
+        
+    ]
+
+    const updateProfile = profile.map((profile, index)=>{
+       return(
+        <li key={index}>
+            <span>{profile.name} </span> <br />
+            <span>{profile.age} </span>
+            <span>{profile.gender} </span>
+        </li>
+        );
+    })
+  return (
+    <div>
+        <ul>
+            {updateProfile}
+        </ul>
+    </div>
+  )
+}
+
+export default Map
