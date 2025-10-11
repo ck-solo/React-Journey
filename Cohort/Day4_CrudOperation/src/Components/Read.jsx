bfc91a7a3041d0fda34f7d133f7aa278460e330b
Lucy <solouser019@gmail.com>
2025-10-11 22:23:56 +0530

    feat: integrate react-hook-form for todo creation and input handling

--- Cohort/Day4_CrudOperation/src/Components/Read.jsx
+++ Cohort/Day4_CrudOperation/src/Components/Read.jsx
@@ -15,9 +15,9 @@ const Read = (props) => {
 
   const updateTodo = todos.map((todos) => {
     return (
-      <li className="flex p-2 rounded-md bg-gray-800  text-3xl justify-between " key={todos.id}>
+      <li className="flex p-2 rounded-md bg-gray-800 mb-5 text-3xl justify-between " key={todos.id}>
        <span className="">{todos.title}</span>  {" "}
-        <span className="text-red-700" onClick={() => DeleteHandler(todos.id)}>Delete</span>{" "}
+        <button className="text-red-800 font-thin cursor-pointer text-2xl" onClick={() => DeleteHandler(todos.id)}>Delete</button>{" "}
       </li>
     );
   });
@@ -25,7 +25,7 @@ const Read = (props) => {
   return (
     <div className="w-[50%]  p-20">
       {/* <h1 className={style.read_list_heading} >Todo List</h1> */}
-      <h1 className="text-6xl mb-10 ">Todo List</h1>
+      <h1 className="text-6xl mb-10 "><span className="text-orange-600">Pending</span> Todo List</h1>
       <ol>{updateTodo}</ol>
     </div>
   );
--- Cohort/Day4_CrudOperation/src/Components/Write.jsx
+++ Cohort/Day4_CrudOperation/src/Components/Write.jsx
@@ -1,35 +1,34 @@
 import React from "react";
-import { useState } from 'react'
+ 
 import { nanoid } from 'nanoid'
+import {useForm} from 'react-hook-form'
 
 const Write = (props) => {
     const todos = props.todos;
-    const settodos = props.settodos;
-    const [title, settitle] = useState("");
+    const settodos = props.settodos; 
+
+    const {register, handleSubmit, reset,formState:{errors}} = useForm()
     
-    const HandleSubmit = (e) =>{
-        e.preventDefault()
     
-        const newtodo = {
-          id: nanoid(),
-          title:title,
-          isCompleted:false
-        }
-        settodos([...todos,newtodo])
-        settitle('')
+    const SubmitHandler = (data) =>{  
+      data.id = nanoid(),
+      data.isCompleted = false
+         
+      const copytodo = [...todos]
+      copytodo.push(data)
+      settodos(copytodo)
+        
+        reset()
       }
     
      
 
   return (
     <div className="w-[50%]  rounded-md   text-center">
-      <h1 className="m-20 text-6xl">Set Reminders for tasks</h1>
-      <form  onSubmit={HandleSubmit}>
-        <input className="w-[80%] border-b-2 text-4xl"
-          onChange={(e) => {
-            settitle(e.target.value);
-          }}
-          value={title}
+      <h1 className="m-20 text-6xl">Set <span className='text-red-800'>Reminders</span> for tasks</h1>
+      <form  onSubmit={handleSubmit(SubmitHandler)}>
+        <input className="w-[80%] border-b-2 text-4xl "
+           {...register('title', {required: true})}
           type="text"
           placeholder="Enter the title"
         />
